46c082426830a53bba9c14f444890ec6166f0a31
sabu <sabu@mail.com>
2015-02-23 20:43:43 -0600

    Adding info for RepeatVisitBolt test

--- src/test/java/storm/cookbook/RepeatVisitBoltTest.java
+++ src/test/java/storm/cookbook/RepeatVisitBoltTest.java
@@ -22,16 +22,25 @@ import backtype.storm.tuple.Values;
 // Start Redis server before executing Test
 // /usr/local/bin/redis-server
 
+// Parameterized is a runner inside JUnit that run same test case for all set of inputs
+// - The class has  single constructor that stores the test data.
+// - The class has a static method that generates and returns test data.
 @RunWith(value = Parameterized.class)
 public class RepeatVisitBoltTest extends StormTestCase {
 
+	// data generator method must be annotated with @Parameters
+	// Each array element represents data to be used in each test execution
+	// Number of elements in each array must be same as number of parameters in constructor
 	@Parameterized.Parameters
 	public static Collection<Object[]> data() {
+		
+		// 2-D array
 		Object[][] data = new Object[][] {
 				{ "192.168.33.100", "Client1", "myintranet.com", "false" },
 				{ "192.168.33.100", "Client1", "myintranet.com", "false" },
 				{ "192.168.33.101", "Client2", "myintranet1.com", "true" },
 				{ "192.168.33.102", "Client3", "myintranet2.com", "false" } };
+		// return list of arrays
 		return Arrays.asList(data);
 	}
 
@@ -52,11 +61,14 @@ public class RepeatVisitBoltTest extends StormTestCase {
 
 	@BeforeClass
 	public static void setupJedis() {
+		
 		Jedis jedis = new Jedis("localhost", 6379);
 		jedis.flushDB();
 		Iterator<Object[]> it = data().iterator();
+		// Before the tests execute, the static data is populated into the Redis DB
 		while (it.hasNext()) {
 			Object[] values = it.next();
+			// In each object, check third element of array,  
 			if (values[3].equals("false")) {
 				String key = values[2] + ":" + values[1];
 				jedis.set(key, "visited");// unique, meaning it must exist
@@ -66,17 +78,26 @@ public class RepeatVisitBoltTest extends StormTestCase {
 
 	@Test
 	public void testExecute() {
+		
 		jedis = new Jedis("localhost", 6379);
+		
+		// test class
 		RepeatVisitBolt bolt = new RepeatVisitBolt();
 		Map<String, String> config = new HashMap<String, String>();
 		config.put("redisHost", "localhost");
 		config.put("redisPort", "6379");
+		
+		// mocked OutputCollector
 		final OutputCollector collector = context.mock(OutputCollector.class);
+		
+		// Initialize bolt, set redis DB
 		bolt.prepare(config, null, collector);
 
 		assertEquals(true, bolt.isConnected());
 
 		final Tuple tuple = getTuple();
+		
+		// define behavior to mock
 		context.checking(new Expectations() {
 			{
 				oneOf(tuple).getStringByField(Fields.IP);
@@ -89,6 +110,7 @@ public class RepeatVisitBoltTest extends StormTestCase {
 			}
 		});
 
+		// test the bolt 
 		bolt.execute(tuple);
 		context.assertIsSatisfied();
 
--- src/test/java/storm/cookbook/StormTestCase.java
+++ src/test/java/storm/cookbook/StormTestCase.java
@@ -5,8 +5,12 @@ import org.jmock.lib.legacy.ClassImposteriser;
 
 import backtype.storm.tuple.Tuple;
 
+// simple abstraction of some of the initialization code
 public class StormTestCase {
 
+	
+	// usually mockeries are called as mockery, or context.
+	// Initialize JMock context and use it to mock any class
 	protected Mockery context = new Mockery() {
 		{
 			setImposteriser(ClassImposteriser.INSTANCE);
@@ -14,6 +18,7 @@ public class StormTestCase {
 	};
 
 	protected Tuple getTuple() {
+		// tuple class mocked
 		final Tuple tuple = context.mock(Tuple.class);
 		return tuple;
 	}
